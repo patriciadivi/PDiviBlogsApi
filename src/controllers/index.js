269f7bb5b3c8381d50c93bda7226667cbe0d6fa4
PatriciaDivi <patriciavianad@gmail.com>
2022-07-18 00:56:14 -0300

    refactor: exportação do arquivo getUserControllerID no index do controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const postLoginController = require('./postLoginController');
 const postUserController = require('./postUserController');
 const getUserControllerAll = require('./getUserControllerAll');
+const getUserControllerID = require('./getUserControllerID');
 
 module.exports = {
     postLoginController,
     postUserController,
     getUserControllerAll,
+    getUserControllerID,
 };
