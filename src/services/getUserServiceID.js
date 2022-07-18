57b8e122b0d6341691baa268b205180a890734e6
PatriciaDivi <patriciavianad@gmail.com>
2022-07-18 01:22:03 -0300

    feat:  Requisito 06 | buscar usuario através do id na rota '/user/:id'

--- src/services/getUserServiceID.js
+++ src/services/getUserServiceID.js
@@ -1,8 +1,12 @@
 const { User } = require('../database/models');
+const { MyError } = require('../utils/errorCustomer');
 
-const getUserServiceAll = async (id) => {
-    const getUserIDModels = await User.findOne({ where: { id } });
+const getUserServiceID = async (id) => {
+    const getUserIDModels = await User.findOne({ where: { id },
+        attributes: ['id', 'displayName', 'email', 'image'],
+    });
+    if (!getUserIDModels) throw new MyError(404, 'User does not exist');
       return getUserIDModels;
 };
 
-module.exports = getUserServiceAll;
+module.exports = getUserServiceID;
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const postLoginService = require('./postLoginService');
 const postUserService = require('./postUserService');
 const getUserServiceAll = require('./getUserServiceAll');
+const getUserServiceID = require('./getUserServiceID');
 
 module.exports = {
     postLoginService,
     postUserService,
     getUserServiceAll,
+    getUserServiceID,
 };
