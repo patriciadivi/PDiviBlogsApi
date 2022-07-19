aa5c461bac0f813b361b2d5705170b67fccb0c5a
PatriciaDivi <patriciavianad@gmail.com>
2022-07-18 21:03:42 -0300

    fix: bug na router

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const controllers = require('../controllers/index');
-// const nameValidation = require('../middlewares/zod/nameValidation');
+const nameValidation = require('../middlewares/zod/nameValidation');
 const { tokenValidation } = require('../middlewares/tokenGlobal');
 
 const categories = Router();
 
-categories.post('/categories',
+categories.post('/categories', nameValidation,
 tokenValidation, controllers.postCategoriesController);
 
 module.exports = categories;
