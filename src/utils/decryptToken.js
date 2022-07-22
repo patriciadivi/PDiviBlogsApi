851a726ca49159fa19aa8db5c7a4fa14ac74fb39
PatriciaDivi <patriciavianad@gmail.com>
2022-07-21 23:59:52 -0300

    feat(Função para decriptar o token): :sparkles: função para decriptar token do usuario.

--- src/utils/decryptToken.js
+++ src/utils/decryptToken.js
@@ -5,9 +5,9 @@ const { MyError } = require('./errorCustomer');
 const decryptTokenfunc = (paramsToken) => {
 try {
   const token = jwt.verify(paramsToken, process.env.JWT_SECRET);
-  const { data } = token;
-  if (!data) throw new MyError(401, 'invalid token');
-  return data;
+  // const { data } = token;
+  if (!token) throw new MyError(401, 'invalid token');
+  return token;
 } catch (error) {
   console.error(error.message);
 }
