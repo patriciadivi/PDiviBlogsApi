372e0ebb96e79c9a8e13f4c68d72f319f89e3805
PatriciaDivi <patriciavianad@gmail.com>
2022-07-22 00:05:36 -0300

    feat(Requisito 12 -  desenvolvida rota para criar novo post): :rocket: rota de post na '/post' criada e finalizada , com criação de um novo post, tabela postCategorie atualizada  com o novo post criado

--- src/services/postPostService.js
+++ src/services/postPostService.js
@@ -1,39 +1,30 @@
-const { Category, BlogPost } = require('../database/models');
+const { Category, BlogPost, PostCategory, sequelize } = require('../database/models');
 const { MyError } = require('../utils/errorCustomer');
 const { decryptTokenfunc } = require('../utils/decryptToken');
 
 const postPostService = async (titleParams, contentParams, categoryIdsParams, authorization) => {
-    const { count } = await Category.findAndCountAll({ 
-        where: { id: categoryIdsParams },
-    });
-
+    const { count } = await Category.findAndCountAll({ where: { id: categoryIdsParams } });
     if (categoryIdsParams.length !== count) throw new MyError(400, '"categoryIds" not found');
     
     const decryptToken = decryptTokenfunc(authorization);
-    // console.log('decryptToken', decryptToken);
-    // const idUserId = decryptToken.data.id;
-    // console.log('idUserId>>>>>>>>', !idUserId);
-    // if (!idUserId) throw new MyError(400, '"idUserId" not found');
-    // const verificationUser = await User.findOne({ where: { id: idUserId } });
-    // const resultUser = verificationUser.dataValues;
-    // const { id: userIdGet } = resultUser;
+    const idUserId = decryptToken.data.id;
 
-    // const resultCreatePost = sequelize.transaction(async (trans) => {
-    //     const createdPost = await BlogPost.create({
-    //         title: titleParams,
-    //         content: contentParams,
-    //         userId: idUserId,
-    //     }, { transaction: trans });
-        
-    //     return createdPost;
-    // });
-    const createNewPost = await BlogPost.create({
+    const resultCreatePost = sequelize.transaction(async (trans) => {
+        const createdPost = await BlogPost.create({
             title: titleParams,
             content: contentParams,
-            userId: decryptToken.id,
-        });
+            userId: idUserId,
+        }, { transaction: trans });
+    
+        const resultCreatePostId = createdPost.dataValues.id;
+
+    const modified = categoryIdsParams
+        .map((elem) => ({ postId: resultCreatePostId, categoryId: elem }));
+    await PostCategory.bulkCreate(modified, { transaction: trans });
+        return createdPost;
+    });
         
-        return createNewPost; 
+        return resultCreatePost; 
 };
 
 module.exports = postPostService;
